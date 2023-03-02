8d39aec904baaf7922c481ce7f71a5084669a0b6
NicolaPlacentino <nyko0921@gmail.com>
2023-03-02 16:37:01 +0100

    fixed typo

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.sh
 
 Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 
-Route::get('/teams/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
+Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
